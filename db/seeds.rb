6d53c7ccb66d922b4f162794412f78f5ccc200c4
Candra Tran <candratran@gmail.com>
2017-03-15 12:09:18 -0700

    reseeded database, fixed emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(name: "Han Solo", email: "hansolo@air2d2.com", password: "chewbacca");
-User.create(name: "Chewbacca", email: "hansolo@air2d2.com", password: "hansolo");
+User.create(name: "Chewbacca", email: "chewbacca@air2d2.com", password: "hansolo");
 User.create(name: "Luke Skywalker", email: "lukeskywalker@air2d2.com", password: "lukeskywalker");
 User.create(name: "Darth Vader", email: "darthvader@air2d2.com", password: "darthvader");
-User.create(name: "Yoda", email: "yoda@air2d2.com", password: "yoda");
+User.create(name: "Yoda", email: "yoda@air2d2.com", password: "yoda1234");
 User.create(name: "Leia Organa", email: "leiaorgana@air2d2.com", password: "leiaorgana");
