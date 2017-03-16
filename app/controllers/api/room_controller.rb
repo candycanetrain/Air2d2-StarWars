76b3aeddf737ab74b17ea64945e38e7f2ee44f24
Candra Tran <candratran@gmail.com>
2017-03-16 16:55:24 -0700

    created controller for room

---
+++ app/controllers/api/room_controller.rb
@@ -0,0 +1,30 @@
+class Api::RoomController < ApplicationController
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      render "api/room/show#{params[:id]}"
+    else
+      render json: @room.errors.full_messages,status: 422
+    end
+  end
+
+  def destroy
+  end
+
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+    @room = Room.find_by_id(params[:id])
+    if @room
+      render "api/room/show#{id}"
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:id, :name, :city, :description, :location, :price, :bedrooms, :beds, :bathrooms, :homeType, :accomdates, :amenities)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'room/create'
+  end
+
+  namespace :api do
+    get 'room/destroy'
+  end
+
+  namespace :api do
+    get 'room/index'
+  end
+
+  namespace :api do
+    get 'room/show'
+  end
+
+  namespace :api do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
   end
