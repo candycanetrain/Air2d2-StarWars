b3dbebfa40a91b3c543f2bc5bbe413eedcf33309
Candra Tran <candratran@gmail.com>
2017-03-16 17:03:55 -0700

    added routes for room

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'room/create'
-  end
-
-  namespace :api do
-    get 'room/destroy'
-  end
-
-  namespace :api do
-    get 'room/index'
-  end
-
-  namespace :api do
-    get 'room/show'
-  end
-
-  namespace :api do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :rooms, only: [:create, :index, :show]
   end
 
   root "static_pages#root"
