8e22578249818f6e6c7f218cec99fb8f4d7d1920
Candra Tran <candratran@gmail.com>
2017-03-21 13:25:01 -0700

    deleted 2 migrations

--- db/migrate/20170318235250_remove_accomdates_from_rooms.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAccomdatesFromRooms < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :rooms, :accomdates, :integer
-  end
-end
--- db/migrate/20170318235457_add_accommodates_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddAccommodatesToRooms < ActiveRecord::Migration[5.0]
-  def change
-    add_column :rooms, :accommodates, :integer, null: false
-  end
-end
