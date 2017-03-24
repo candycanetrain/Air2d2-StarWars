6958a7f1abd933c3076638afb19428f03c1a3f67
Candra Tran <candratran@gmail.com>
2017-03-23 18:14:56 -0700

    remove debugger for heroku

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -26,7 +26,7 @@ export const fetchReview = (id) => dispatch => {
 };
 
 export const createReview = review => dispatch => {
-  debugger
+  // debugger
   return(
     ReviewUtil.createReview(review)
     .then(review => dispatch(receiveReview(review)),
