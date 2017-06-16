6f3bea5a641580a52cd2441ca2b65680499c27a1
Candra Tran <candratran@gmail.com>
2017-06-15 23:16:15 -0700

    added tests for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-
+  
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:session_token) }
 
+  describe "associations" do
+    # "it" refers to an instance of the User class here because we have not explicitly specified a subject
+    it { should have_many(:bookings)}
+    it { should have_many(:reviews)}
+  end
 end
